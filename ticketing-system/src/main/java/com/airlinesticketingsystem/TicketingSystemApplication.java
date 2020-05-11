1c6dbee0100b5c804a2419dc85c6ea0b7d4d7efa
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-11 15:17:03 +0300

    First Create Spring Boot Project

---
+++ ticketing-system/src/main/java/com/airlinesticketingsystem/TicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.airlinesticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemApplication.class, args);
+	}
+
+}
