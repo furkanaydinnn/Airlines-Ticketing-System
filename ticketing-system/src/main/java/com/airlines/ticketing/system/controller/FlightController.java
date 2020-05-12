2d403bd1986a6d6f8cc9472d1cf72636e72dbc27
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-12 21:14:43 +0300

    Flight Controller

--- ticketing-system/src/main/java/com/airlines/ticketing/system/controller/FlightController.java
+++ ticketing-system/src/main/java/com/airlines/ticketing/system/controller/FlightController.java
@@ -3,7 +3,6 @@ package com.airlines.ticketing.system.controller;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
